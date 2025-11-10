76a437b396ae1b18fb04d89e1091edcb3a44a671
Gustavo Wandeur <guwandeur@gmail.com>
2025-11-10 18:42:26 -0300

    Simplificação do código (para usar apenas uma rota para criar e vizualizar a postagem)

--- first_flask_app/app.py
+++ first_flask_app/app.py
@@ -22,19 +22,16 @@ def posts_page(post_id):
     return render_template("post.jinja2", post=post)
 
 
-@app.route("/post/form")
-def form():
-    return render_template("create.jinja2")
-
-
-@app.route("/post/create", methods=["POST"])
+@app.route("/post/create", methods=["GET", "POST"])
 def create():
-    title = request.form.get("title")
-    content = request.form.get("content")
-    post_id = len(posts)
-    posts[post_id] = {"id": post_id, "title": title, "content": content}
+    if request.method == "POST":
+        title = request.form.get("title")
+        content = request.form.get("content")
+        post_id = len(posts)
+        posts[post_id] = {"id": post_id, "title": title, "content": content}
 
-    return redirect(url_for("posts_page", post_id=post_id))
+        return redirect(url_for("posts_page", post_id=post_id))
+    return render_template("create.jinja2")
 
 
 if __name__ == "__main__":
