3d12999faf0fd8f6bcfbf241ab66447bf3bd47dd
Gustavo Wandeur <guwandeur@gmail.com>
2025-11-10 18:29:46 -0300

    primeiro formulário em flask

--- first_flask_app/app.py
+++ first_flask_app/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
-posts = {0: {"title": "Hello, world", "content": "This is my first blog post!"}}
+posts = {
+    0: {"post_id": 0, "title": "Hello, world", "content": "This is my first blog post!"}
+}
 
 
 @app.route("/")
@@ -18,6 +20,21 @@ def posts_page(post_id):
             "404.jinja2", message=f"A post with id {post_id} was not found."
         )
     return render_template("post.jinja2", post=post)
+
+
+@app.route("/post/form")
+def form():
+    return render_template("create.jinja2")
+
+
+@app.route("/post/create")
+def create():
+    title = request.args.get("title")
+    content = request.args.get("content")
+    post_id = len(posts)
+    posts[post_id] = {"id": post_id, "title": title, "content": content}
+
+    return redirect(url_for("posts_page", post_id=post_id))
 
 
 if __name__ == "__main__":
